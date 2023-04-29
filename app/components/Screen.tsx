acb82c6d4643992c46ed7badb5aaabcf9233d3bd
Sandipan Mahata <Sandipan.m@muvin.in>
2023-04-29 09:54:39 +0530

    added Screen component

---
+++ app/components/Screen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import Constants from "expo-constants";
+
+export default function Screen({ children }: { children: React.ReactNode }) {
+	return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+	container: { flex: 1, paddingTop: Constants.statusBarHeight },
+});
---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Screen from "../components/Screen";
+
+export default function HomeScreen() {
+	return (
+		<Screen>
+			<Text>hello</Text>
+		</Screen>
+	);
+}
+
+const styles = StyleSheet.create({});
