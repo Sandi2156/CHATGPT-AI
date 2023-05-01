f29a8b6b5b8646d7029610779640d24c8b05649a
Sandipan Mahata <Sandipan.m@muvin.in>
2023-05-01 08:25:52 +0530

    added api in chat component to talk to chat gpt

--- app/api/client.ts
+++ app/api/client.ts
@@ -4,7 +4,7 @@ const apiClient = create({
 	baseURL: "https://api.openai.com/v1/chat/completions",
 	headers: {
 		"Content-Type": "application/json",
-		Authorization: "Bearer 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",
+		Authorization: "Bearer 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",
 	},
 });
 
--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -3,22 +3,56 @@ import { GiftedChat } from "react-native-gifted-chat";
 import { Ionicons } from "@expo/vector-icons";
 import AppText from "./AppText";
 import { View } from "react-native";
+import uuid from "react-native-uuid";
+
+import chatApi from "../api/chat";
 
 type MessageType = {
-	_id: number;
+	_id: number | string;
 	text: string;
 	createdAt: Date;
 	user: { _id: number; name: string; avatar: string };
 };
 
+type GptRequestMessageType = { role: string; content: string };
+
 function Chat() {
-	const [messages, setMessages] = useState<Array<MessageType>>([]);
+	const [chatMessages, setChatMessages] = useState<Array<MessageType>>([]);
+	const [gptRequestMessages, setGptRequestMessages] = useState<
+		Array<GptRequestMessageType>
+	>([]);
+
+	const sendToChatGpt = async (content: string) => {
+		const reqMessages = [...gptRequestMessages, { role: "user", content }];
+
+		const response: any = await chatApi.getResponseChat(
+			"gpt-3.5-turbo",
+			reqMessages
+		);
+		if (!response.ok) return;
+
+		setGptRequestMessages(reqMessages);
+		setChatMessages((previousMessages) =>
+			GiftedChat.append(previousMessages, [
+				{
+					_id: `${uuid.v4()}`,
+					createdAt: new Date(),
+					text: response.data?.choices[0].message.content,
+					user: {
+						_id: 2,
+						name: "React Native",
+						avatar: "https://placeimg.com/140/140/any",
+					},
+				},
+			])
+		);
+	};
 
 	useEffect(() => {
-		setMessages([
+		setChatMessages([
 			{
 				_id: 1,
-				text: "Hello  alsdjfklasjdfkljskldfjklsdjklfjkl asdlkfjklsdjfkljsdklfjlsdj lajfklasdjfkljsadklfj lkajsdklfjasdklfjklsadjf klasjdfkladjsfklasjdf lasjdfklasdjfklsdj fklklasfjdklsdjfklj lkajsdfkljasdklfj ljasdfkljsdfklj",
+				text: "Hello, How can I help you today ?",
 				createdAt: new Date(),
 				user: {
 					_id: 2,
@@ -30,22 +64,20 @@ function Chat() {
 	}, []);
 
 	const onSend = useCallback((messages: Array<MessageType> = []) => {
-		console.log(messages);
-		setMessages((previousMessages) =>
+		setChatMessages((previousMessages) =>
 			GiftedChat.append(previousMessages, messages)
 		);
+		sendToChatGpt(messages[messages.length - 1].text);
 	}, []);
 
 	return (
 		<GiftedChat
-			messages={messages}
+			messages={chatMessages}
 			onSend={(messages) => onSend(messages)}
 			user={{
 				_id: 1,
 				avatar: () => <Ionicons name="person-outline" size={20} />,
 			}}
-			showUserAvatar={true}
-			showAvatarForEveryMessage
 			// renderMessage={(message) => (
 			// 	<View>
 			// 		<AppText>{message.currentMessage?.text}</AppText>
