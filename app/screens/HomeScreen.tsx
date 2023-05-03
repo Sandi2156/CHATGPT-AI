f2fa20966908788b76c22147b8bf364a99394a48
Sandipan Mahata <Sandipan.m@muvin.in>
2023-05-03 12:13:06 +0530

    sdfj

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import Screen from "../components/Screen";
 import Banner from "../components/Banner";
 import HomeSectionButton from "../components/HomeSectionButton";
-import ItemSeperator from "../components/ItemSeperator";
 import FloatingButton from "../components/FloatingButton";
 import SuggestionHome from "../components/SuggestionHome";
 
@@ -12,20 +11,24 @@ import buttonList from "../data/buttonList";
 import suggestionsHomeList from "../data/suggestionsHomeList";
 
 export default function HomeScreen() {
+	const one = buttonList.slice(0, 4);
+	const two = buttonList.slice(4);
+
 	return (
-		<Screen isScrollable={true}>
+		<Screen isScrollable>
 			<Banner />
 
 			<View style={styles.listContainer}>
-				<FlatList
-					data={buttonList}
-					keyExtractor={(item) => item.title}
-					renderItem={({ item }) => <HomeSectionButton {...item} />}
-					numColumns={4}
-					columnWrapperStyle={{ flex: 1, justifyContent: "space-around" }}
-					ItemSeparatorComponent={ItemSeperator}
-					scrollEnabled={false}
-				/>
+				<View style={styles.row}>
+					{one.map((item, index) => (
+						<HomeSectionButton key={index} {...item} />
+					))}
+				</View>
+				<View style={styles.row}>
+					{two.map((item, index) => (
+						<HomeSectionButton key={index} {...item} />
+					))}
+				</View>
 			</View>
 
 			<View style={styles.suggestionContainer}>
@@ -34,12 +37,17 @@ export default function HomeScreen() {
 				))}
 			</View>
 
-			<FloatingButton />
+			{/* <FloatingButton /> */}
 		</Screen>
 	);
 }
 
 const styles = StyleSheet.create({
 	listContainer: { paddingHorizontal: 20, marginVertical: 40 },
-	suggestionContainer: {},
+	suggestionContainer: { marginBottom: 20 },
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		marginBottom: 10,
+	},
 });
